16c08d645e7906f60f8971da82129df50cf699f7
sarah <>
2021-09-01 16:34:47 +0200

    simplify osqp benchmark

--- benchmark/src/osqp.cpp
+++ benchmark/src/osqp.cpp
@@ -161,94 +161,79 @@ auto main() -> int {
 	auto A_eigen = ldlt_test::rand::sparse_matrix_rand<Scalar>(n_eq, dim, p);
 	auto g_eigen = ldlt_test::rand::vector_rand<Scalar>(dim);
 	auto b_eigen = ldlt_test::rand::vector_rand<Scalar>(n_eq);
-
-	auto H = ldlt_test::eigen_to_osqp_mat(H_eigen);
-	auto A = ldlt_test::eigen_to_osqp_mat(A_eigen);
-	auto osqp = OSQPData{
-			dim,
-			n_eq,
-			&H,
-			&A,
-			g_eigen.data(),
-			b_eigen.data(),
-			b_eigen.data(),
-	};
-
-	OSQPSettings osqp_settings{};
-	osqp_set_default_settings(&osqp_settings);
-	osqp_settings.eps_rel = 1e-9;
-	osqp_settings.eps_abs = 1e-9;
-
-	OSQPWorkspace* osqp_work{};
+	i32 n_iter = 100;
 
 	{
-		{ LDLT_DECL_SCOPE_TIMER("osqp bench", "osqp total"); }
-		{ LDLT_DECL_SCOPE_TIMER("osqp bench", "osqp solve"); }
+		{ LDLT_DECL_SCOPE_TIMER("osqp bench", "osqp"); }
 		{ LDLT_DECL_SCOPE_TIMER("osqp bench", "ours"); }
 		(void)0;
 	}
-
 	{
-		LDLT_DECL_SCOPE_TIMER("osqp bench", "osqp total");
+		auto H = ldlt_test::eigen_to_osqp_mat(H_eigen);
+		auto A = ldlt_test::eigen_to_osqp_mat(A_eigen);
+		auto osqp = OSQPData{
+				dim,
+				n_eq,
+				&H,
+				&A,
+				g_eigen.data(),
+				b_eigen.data(),
+				b_eigen.data(),
+		};
+
+		OSQPSettings osqp_settings{};
+		osqp_set_default_settings(&osqp_settings);
+		osqp_settings.eps_rel = 1e-9;
+		osqp_settings.eps_abs = 1e-9;
+		osqp_settings.warm_start = 0;
+		osqp_settings.verbose = 0;
+
+		OSQPWorkspace* osqp_work{};
+
 		osqp_setup(&osqp_work, &osqp, &osqp_settings);
-		{
-			LDLT_DECL_SCOPE_TIMER("osqp bench", "osqp solve");
-			osqp_solve(osqp_work);
+
+		LDLT_DECL_SCOPE_TIMER("osqp bench", "osqp");
+		for (i32 i = 0; i < n_iter; ++i) {
+			{ osqp_solve(osqp_work); }
 		}
+
+		osqp_cleanup(osqp_work);
 	}
 
-	auto x = Eigen::Map<Vec<Scalar>>(osqp_work->solution->x, dim);
-	auto y = Eigen::Map<Vec<Scalar>>(osqp_work->solution->y, n_eq);
-
-	fmt::print(
-			"{} {}\n",
-			(H_eigen.selfadjointView<Eigen::Upper>() * x + g_eigen +
-	     A_eigen.transpose() * y)
-					.lpNorm<Eigen::Infinity>(),
-			(A_eigen * x - b_eigen).lpNorm<Eigen::Infinity>());
-
-	auto n_iters = [&] {
-		x.setZero();
-		y.setZero();
+	{
+		Vec<Scalar> x(dim);
+		Vec<Scalar> y(n_eq);
 
 		auto H = Mat<Scalar>(H_eigen.toDense().selfadjointView<Eigen::Upper>());
 		auto A = Mat<Scalar>(A_eigen.toDense());
-		auto ruiz = qp::preconditioner::IdentityPrecond{};
+		auto precond = qp::preconditioner::IdentityPrecond{};
 
-		LDLT_DECL_SCOPE_TIMER("osqp bench", "ours");
+		{
+			LDLT_DECL_SCOPE_TIMER("osqp bench", "ours");
+			for (i32 i = 0; i < n_iter; ++i) {
+				x.setZero();
+				y.setZero();
 
-		return qp::detail::solve_qp(
-							 VectorViewMut<Scalar>{x.data(), i32(x.rows())},
-							 VectorViewMut<Scalar>{y.data(), i32(y.rows())},
-							 qp::QpView<Scalar, colmajor, colmajor>{
-									 detail::from_eigen_matrix(H),
-									 detail::from_eigen_vector(g_eigen),
-									 detail::from_eigen_matrix(A),
-									 detail::from_eigen_vector(b_eigen),
-									 {},
-									 {},
-							 },
-							 1000,
-							 1e-9,
-							 0,
-							 LDLT_FWD(ruiz))
-		    .n_iters;
-	}();
-	fmt::print("{}\n", n_iters);
-
-	fmt::print(
-			"{} {}\n",
-			(H_eigen.selfadjointView<Eigen::Upper>() * x + g_eigen +
-	     A_eigen.transpose() * y)
-					.lpNorm<Eigen::Infinity>(),
-			(A_eigen * x - b_eigen).lpNorm<Eigen::Infinity>());
+				qp::detail::solve_qp(
+						VectorViewMut<Scalar>{x.data(), i32(x.rows())},
+						VectorViewMut<Scalar>{y.data(), i32(y.rows())},
+						qp::QpView<Scalar, colmajor, colmajor>{
+								detail::from_eigen_matrix(H),
+								detail::from_eigen_vector(g_eigen),
+								detail::from_eigen_matrix(A),
+								detail::from_eigen_vector(b_eigen),
+								{},
+								{},
+						},
+						1000,
+						1e-9,
+						0,
+						LDLT_FWD(precond));
+			};
+		}
+	}
 
 	for (auto& outer : LDLT_GET_MAP()["osqp bench"]) {
-		fmt::print("{}: {}\n", outer.first, outer.second.ref);
+		fmt::print("{}: {}\n", outer.first, outer.second.ref.back() / n_iter);
 	}
-	for (auto& outer : LDLT_GET_MAP(Scalar)["eq solver"]) {
-		fmt::print("{}: {}\n", outer.first, outer.second.ref);
-	}
-
-	osqp_cleanup(osqp_work);
 }
