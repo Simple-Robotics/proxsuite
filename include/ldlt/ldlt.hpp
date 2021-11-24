113616ffe8318a017c24e37c2abb2b4953b4b295
Justin Carpentier <justin.carpentier@inria.fr>
2021-11-24 12:06:05 +0100

    ldlt: remove online allocation

--- include/ldlt/ldlt.hpp
+++ include/ldlt/ldlt.hpp
@@ -7,6 +7,8 @@
 #include "ldlt/solve.hpp"
 #include "ldlt/update.hpp"
 
+#include <iostream>
+
 namespace ldlt {
 
 LDLT_DEFINE_TAG(decompose, Decompose);
@@ -23,64 +25,104 @@ private:
 	using LView = Eigen::TriangularView<
 			Eigen::Map< //
 					ColMat const,
-					Eigen::Unaligned,
+					Eigen::Aligned,
 					Eigen::OuterStride<DYN>>,
 			Eigen::UnitLower>;
 	using LViewMut = Eigen::TriangularView<
 			Eigen::Map< //
 					ColMat,
-					Eigen::Unaligned,
+					Eigen::Aligned,
 					Eigen::OuterStride<DYN>>,
 			Eigen::UnitLower>;
 
 	using LTView = Eigen::TriangularView<
 			Eigen::Map< //
 					RowMat const,
-					Eigen::Unaligned,
+					Eigen::Aligned,
 					Eigen::OuterStride<DYN>>,
 			Eigen::UnitUpper>;
 	using LTViewMut = Eigen::TriangularView<
 			Eigen::Map< //
 					RowMat,
-					Eigen::Unaligned,
+					Eigen::Aligned,
 					Eigen::OuterStride<DYN>>,
 			Eigen::UnitUpper>;
 
-	using VecMap = Eigen::Map<Vec const>;
-	using VecMapMut = Eigen::Map<Vec>;
+	using ColMatMap = Eigen::Map<ColMat const, Eigen::Aligned, Eigen::OuterStride<DYN>>;
+	using ColMatMapMut = Eigen::Map<ColMat, Eigen::Aligned, Eigen::OuterStride<DYN>>;
+
+	using RowMatMap = Eigen::Map<RowMat const, Eigen::Aligned, Eigen::OuterStride<DYN>>;
+	using RowMatMapMut = Eigen::Map<RowMat, Eigen::Aligned, Eigen::OuterStride<DYN>>;
+
+	using VecMap = Eigen::Map<Vec const, Eigen::Aligned>;
+	using VecMapMut = Eigen::Map<Vec, Eigen::Aligned>;
 
 	using VecMapISize = Eigen::Map<Eigen::Matrix<isize, DYN, 1> const>;
 	using Perm = Eigen::PermutationWrapper<VecMapISize>;
 
-	ColMat _l;
-	Vec _d;
+	ColMat _l, _new_l;
+	Vec _d, _new_d, _permuted_a;
 	std::vector<isize> perm;
 	std::vector<isize> perm_inv;
+	isize dim, max_dim;
 
 public:
-	Ldlt(ReserveUninit /*tag*/, isize dim)
+  
+  virtual ~Ldlt()
+  {
+//    std::cout << "_d" << _d.transpose() << std::endl;
+//    _l.~ColMat();
+  }
+	Ldlt(ReserveUninit /*tag*/, isize dim, isize _max_dim = 0)
 			: //
-				_l(dim, dim),
-				_d(dim),
-				perm(usize(dim)),
-				perm_inv(usize(dim)) {}
-	Ldlt(Decompose /*tag*/, ColMat mat)
-			: _l(LDLT_FWD(mat)),
-				_d(_l.rows()),
-				perm(usize(_l.rows())),
-				perm_inv(usize(_l.rows())) {
+				dim(dim) 
+	{
+		if(_max_dim == 0)
+			max_dim = dim;
+		else
+			max_dim = _max_dim;
+
+		_l.resize(max_dim,max_dim);
+    _new_l.resize(max_dim,max_dim);
+		_d.resize(max_dim);
+    _new_d.resize(max_dim);
+    _permuted_a.resize(max_dim);
+
+		perm.reserve(usize(max_dim)); perm.resize(usize(dim));
+		perm_inv.reserve(usize(max_dim)); perm_inv.resize(usize(dim));
+	}
+
+	Ldlt(Decompose /*tag*/, ColMat mat, isize _max_dim = 0)
+			: //
+				dim(mat.rows()) 
+	{
+		if(_max_dim == 0)
+			max_dim = dim;
+		else
+			max_dim = _max_dim;
+
+		_l.resize(max_dim,max_dim);
+		_l.topLeftCorner(dim,dim) = mat;
+    _new_l.resize(max_dim,max_dim);
+		_d.resize(max_dim);
+    _new_d.resize(max_dim);
+    _permuted_a.resize(max_dim);
+
+		perm.reserve(usize(max_dim)); perm.resize(usize(dim));
+		perm_inv.reserve(usize(max_dim)); perm_inv.resize(usize(dim));
+		
 		this->factorize(_l);
 	}
 
-	auto p() -> Perm { return Perm(VecMapISize(perm.data(), _l.rows())); }
-	auto pt() -> Perm { return Perm(VecMapISize(perm_inv.data(), _l.rows())); }
+	auto p() -> Perm { return Perm(VecMapISize(perm.data(), l().rows())); }
+	auto pt() -> Perm { return Perm(VecMapISize(perm_inv.data(), l().rows())); }
 
 	auto reconstructed_matrix() const -> ColMat {
 		isize n = _d.rows();
 		auto tmp = ColMat(n, n);
-		tmp = l();
+		tmp = ltri();
 		tmp = tmp * _d.asDiagonal();
-		auto A = ColMat(tmp * lt());
+		auto A = ColMat(tmp * ltrit());
 
 		for (isize i = 0; i < n; i++) {
 			tmp.row(i) = A.row(perm_inv[usize(i)]);
@@ -91,68 +133,109 @@ public:
 		return A;
 	}
 
-	auto l() const noexcept -> LView {
+	auto l() const noexcept -> ColMatMap {
 		return Eigen::Map< //
 							 ColMat const,
-							 Eigen::Unaligned,
+							 Eigen::Aligned,
 							 Eigen::OuterStride<DYN>>(
-							 _l.data(), _l.rows(), _l.cols(), _l.outerStride())
-		    .template triangularView<Eigen::UnitLower>();
+							 _l.data(), dim, dim, _l.outerStride());
 	}
-	auto l_mut() noexcept -> LViewMut {
+
+	auto l_mut() noexcept -> ColMatMapMut {
 		return Eigen::Map< //
 							 ColMat,
-							 Eigen::Unaligned,
+							 Eigen::Aligned,
 							 Eigen::OuterStride<DYN>>(
-							 _l.data(), _l.rows(), _l.cols(), _l.outerStride())
+							 _l.data(), dim, dim, _l.outerStride());
+	}
+  
+  auto new_l_mut(isize n) noexcept -> ColMatMapMut {
+    return Eigen::Map< //
+               ColMat,
+               Eigen::Aligned,
+               Eigen::OuterStride<DYN>>(
+               _new_l.data(), n, n, _new_l.outerStride());
+  }
+
+	auto ltri() const noexcept -> LView {
+		return Eigen::Map< //
+							 ColMat const,
+							 Eigen::Aligned,
+							 Eigen::OuterStride<DYN>>(
+							 _l.data(), dim, dim, _l.outerStride())
 		    .template triangularView<Eigen::UnitLower>();
 	}
-	auto lt() const noexcept -> LTView {
+	auto ltri_mut() noexcept -> LViewMut {
+		return Eigen::Map< //
+							 ColMat,
+							 Eigen::Aligned,
+							 Eigen::OuterStride<DYN>>(
+							 _l.data(), dim, dim, _l.outerStride())
+		    .template triangularView<Eigen::UnitLower>();
+	}
+	auto lt() const noexcept -> RowMatMap {
 		return Eigen::Map< //
 							 RowMat const,
-							 Eigen::Unaligned,
+							 Eigen::Aligned,
 							 Eigen::OuterStride<DYN>>(
-							 _l.data(), _l.rows(), _l.cols(), _l.outerStride())
+							 _l.data(), dim, dim, _l.outerStride());
+	}
+	auto ltrit() const noexcept -> LTView {
+		return Eigen::Map< //
+							 RowMat const,
+							 Eigen::Aligned,
+							 Eigen::OuterStride<DYN>>(
+							 _l.data(), dim, dim, _l.outerStride())
 		    .template triangularView<Eigen::UnitUpper>();
 	}
 	auto lt_mut() noexcept -> LTViewMut {
 		return Eigen::Map< //
 							 RowMat,
-							 Eigen::Unaligned,
+							 Eigen::Aligned,
 							 Eigen::OuterStride<DYN>>(
-							 _l.data(), _l.rows(), _l.cols(), _l.outerStride())
+							 _l.data(), dim, dim, _l.outerStride())
 		    .template triangularView<Eigen::UnitUpper>();
 	}
-	auto d() const noexcept -> VecMap { return VecMap(_d.data(), _d.rows()); }
-	auto d_mut() noexcept -> VecMapMut { return VecMapMut(_d.data(), _d.rows()); }
+	auto d() const noexcept -> VecMap { return VecMap(_d.data(), dim); }
+	auto d_mut() noexcept -> VecMapMut { return VecMapMut(_d.data(), dim); }
+  auto new_d(isize n) const noexcept -> VecMap { return VecMap(_new_d.data(), n); }
+  auto new_d_mut(isize n) noexcept -> VecMapMut { return VecMapMut(_new_d.data(), n); }
+  auto permuted_a_mut(isize n) noexcept -> VecMapMut { return VecMapMut(_permuted_a.data(), n); }
+  auto permuted_a(isize n) const noexcept -> VecMap { return VecMap(_permuted_a.data(), n); }
+
 
 	void factorize_work(Eigen::Ref<ColMat const> mat, Eigen::Ref<ColMat> work) {
 		isize n = mat.rows();
 		if (_l.rows() != mat.rows()) {
-			_d.resize(n);
-			_l.resize(n, n);
+			dim = n;
+			max_dim = std::max(n,max_dim);
+
+			_d.conservativeResize(max_dim);
+			_l.conservativeResize(max_dim, max_dim);
 			perm.resize(usize(n));
 			perm_inv.resize(usize(n));
 		}
 		if (_l.data() != mat.data()) {
-			_l = mat;
+			l_mut() = mat;
 		}
 
 		ldlt::detail::compute_permutation<T>(
-				perm.data(), perm_inv.data(), {from_eigen, _l.diagonal()});
+				perm.data(), perm_inv.data(), {from_eigen, l_mut().diagonal()});
 
 		ldlt::detail::apply_permutation_sym_work<T>(
-				{from_eigen, _l}, perm.data(), {from_eigen, work}, -1);
+				{from_eigen, l_mut()}, perm.data(), {from_eigen, work}, -1);
 
 		ldlt::factorize(
-				LdltViewMut<T>{{from_eigen, _l}, {from_eigen, _d}},
-				MatrixView<T, colmajor>{from_eigen, _l});
+				LdltViewMut<T>{{from_eigen, l_mut()}, {from_eigen, d_mut()}},
+				MatrixView<T, colmajor>{from_eigen, l_mut()});
 	}
 
 	void factorize(Eigen::Ref<ColMat const> mat) {
 		isize n = mat.rows();
-		LDLT_WORKSPACE_MEMORY(work, Uninit, Mat(n, n), LDLT_CACHELINE_BYTES, T);
-		factorize_work(mat, work.to_eigen());
+		// LDLT_WORKSPACE_MEMORY(work, Uninit, Mat(n, n), LDLT_CACHELINE_BYTES, T);
+		// factorize_work(mat, work.to_eigen());
+		auto work = new_l_mut(n);
+		factorize_work(mat, work);
 	}
 
 	void solve_in_place_work(Eigen::Ref<Vec> rhs, Eigen::Ref<Vec> work) const {
@@ -162,8 +245,8 @@ public:
 		ldlt::solve(
 				{from_eigen, work},
 				LdltView<T>{
-						{from_eigen, _l},
-						{from_eigen, _d},
+						{from_eigen, l()},
+						{from_eigen, d()},
 				},
 				{from_eigen, work});
 		ldlt::detail::apply_perm_rows<T>::fn(
@@ -171,28 +254,32 @@ public:
 	}
 
 	void solve_in_place(Eigen::Ref<Vec> rhs) const {
-		isize n = _l.rows();
-		LDLT_WORKSPACE_MEMORY(work, Uninit, Vec(n), LDLT_CACHELINE_BYTES, T);
-		solve_in_place_work(rhs, work.to_eigen());
+		isize n = l().rows();
+		// LDLT_WORKSPACE_MEMORY(work, Uninit, Vec(n), LDLT_CACHELINE_BYTES, T);
+		auto work = const_cast<Ldlt&>(*this).new_d_mut(n);
+		// solve_in_place_work(rhs, work.to_eigen());
+		solve_in_place_work(rhs, work);
 	}
 
 	void rank_one_update(Eigen::Ref<Vec const> z, T alpha) {
-		LDLT_WORKSPACE_MEMORY(
-				z_work, Uninit, Vec(z.rows()), LDLT_CACHELINE_BYTES, T);
+		// LDLT_WORKSPACE_MEMORY(
+		// 		z_work, Uninit, Vec(z.rows()), LDLT_CACHELINE_BYTES, T);
 
+		auto z_work = new_d_mut(z.rows());
 		isize n = isize(perm.size());
 		for (isize i = 0; i < n; ++i) {
 			z_work(i) = z.data()[perm[usize(i)]];
 		}
 		LdltViewMut<T> ld = {
-				{from_eigen, _l},
-				{from_eigen, _d},
+				{from_eigen, l_mut()},
+				{from_eigen, d_mut()},
 		};
 
 		rank1_update( //
 				ld,
 				ld.as_const(),
-				z_work.as_const(),
+				// z_work.as_const(),
+				{from_eigen,z_work},
 				alpha);
 	}
 
@@ -207,8 +294,13 @@ public:
 		isize i_actual = n;
 
 		{
-			LDLT_WORKSPACE_MEMORY(
-					permuted_a, Uninit, Vec(n + 1), LDLT_CACHELINE_BYTES, T);
+//			LDLT_WORKSPACE_MEMORY(
+//					permuted_a, Uninit, Vec(n + 1), LDLT_CACHELINE_BYTES, T);
+
+//			LDLT_WORKSPACE_MEMORY(
+//					new_l, Uninit, Mat(n + 1, n +1), LDLT_CACHELINE_BYTES, T);
+//			LDLT_WORKSPACE_MEMORY(
+//					new_d, Uninit, Vec(n + 1), LDLT_CACHELINE_BYTES, T);
 
 			for (isize k = 0; k < n; ++k) {
 				auto& p_k = perm[usize(k)];
@@ -225,25 +317,37 @@ public:
 			perm.insert(perm.begin() + i_actual, i);
 			perm_inv.insert(perm_inv.begin() + i, i_actual);
 
+      auto permuted_a = permuted_a_mut(n+1);
 			for (isize k = 0; k < n + 1; ++k) {
 				permuted_a(k) = a(perm[usize(k)]);
 			}
 
-			auto new_l = ColMat(n + 1, n + 1);
-			auto new_d = Vec(n + 1);
+			// auto new_l = ColMat(n + 1, n + 1);
+			// auto new_d = Vec(n + 1);
+      
 			row_append(
 					LdltViewMut<T>{
-							{from_eigen, new_l},
-							{from_eigen, new_d},
+        // {from_eigen, new_l},
+        // {from_eigen, new_d},
+//							{from_eigen, new_l.to_eigen()},
+//							{from_eigen, new_d.to_eigen()},
+        {from_eigen, new_l_mut(n+1)},
+        {from_eigen, new_d_mut(n+1)},
 					},
 					LdltView<T>{
-							{from_eigen, _l},
-							{from_eigen, _d},
+							{from_eigen, l_mut()},
+							{from_eigen, d_mut()},
 					},
-					permuted_a.as_const());
+                 {from_eigen, _permuted_a.head(n+1)}
+                 );
 
-			_l = LDLT_FWD(new_l);
-			_d = LDLT_FWD(new_d);
+			dim += 1;
+      // l_mut() = LDLT_FWD(new_l);
+      // d_mut() = LDLT_FWD(new_d);
+//			l_mut() = new_l.to_eigen();
+//			d_mut() = new_d.to_eigen();
+      l_mut() = new_l_mut(n+1);
+      d_mut() = new_d_mut(n+1);
 		}
 	}
 
@@ -254,23 +358,39 @@ public:
 		isize n = isize(perm.size());
 		isize i_actual = perm_inv[usize(i)];
 
+//		LDLT_WORKSPACE_MEMORY(
+//			new_l, Uninit, Mat(n - 1, n - 1), LDLT_CACHELINE_BYTES, T);
+//		LDLT_WORKSPACE_MEMORY(
+//			new_d, Uninit, Vec(n - 1), LDLT_CACHELINE_BYTES, T);
+
 		{
-			auto new_l = ColMat(n - 1, n - 1);
-			auto new_d = Vec(n - 1);
+			// auto new_l = ColMat(n - 1, n - 1);
+			// auto new_d = Vec(n - 1);
 
 			row_delete(
 					LdltViewMut<T>{
-							{from_eigen, new_l},
-							{from_eigen, new_d},
+        // {from_eigen, new_l},
+        // {from_eigen, new_d},
+//							{from_eigen, new_l.to_eigen()},
+//							{from_eigen, new_d.to_eigen()},
+        {from_eigen, new_l_mut(n-1)},
+        {from_eigen, new_d_mut(n-1)},
 					},
 					LdltView<T>{
-							{from_eigen, _l},
-							{from_eigen, _d},
+							{from_eigen, l_mut()},
+							{from_eigen, d_mut()},
 					},
 					i_actual);
 
-			_l = LDLT_FWD(new_l);
-			_d = LDLT_FWD(new_d);
+//      std::cout << "new_l: " << new_l.to_eigen()(0,0) << std::endl;
+//      std::cout << "new_d: " << new_d.to_eigen()[0] << std::endl;
+			dim -= 1;
+      // l_mut() = LDLT_FWD(new_l);
+      // d_mut() = LDLT_FWD(new_d);
+      //      l_mut() = new_l.to_eigen();
+      //      d_mut() = new_d.to_eigen();
+      l_mut() = new_l_mut(n-1);
+      d_mut() = new_d_mut(n-1);
 
 			perm.erase(perm.begin() + i_actual);
 			perm_inv.erase(perm_inv.begin() + i);
