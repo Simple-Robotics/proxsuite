a3b5e2a93ac19bf17a6ee69f4465ae92ae510f0f
Justin Carpentier <justin.carpentier@inria.fr>
2021-12-10 11:15:25 +0100

    parser: fix linkage and content issues

--- benchmark/src/parser.cpp
+++ benchmark/src/parser.cpp
@@ -1,15 +1,14 @@
 #include <numeric>
 #include <matioCpp/matioCpp.h>
 
+#include <Eigen/Core>
+
 using Scalar = double;
 
 auto main() -> int {
 	using Vec = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
 
-	using ldlt::i64;
-	using ldlt::isize;
+  matioCpp::File input("/home/antoine/Bureau/thèse/projects/prox-qp/solver_f/solver_biding_test/maros_meszaros_data/AUG2DC.mat");
 
-    matioCpp::File input("/home/antoine/Bureau/thèse/projects/prox-qp/solver_f/solver_biding_test/maros_meszaros_data/AUG2DC.mat");
-    
 }
- 
+
