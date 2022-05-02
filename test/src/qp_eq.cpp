cfc42370afa9ae6c8f17d87e83e655c2c9221ae4
BAMBADE Antoine <antoine.bambade@inria.fr>
2022-05-02 15:50:03 +0200

    add random lps tests

--- test/src/qp_eq.cpp
+++ test/src/qp_eq.cpp
@@ -101,3 +101,50 @@ DOCTEST_TEST_CASE("sparse random strongly convex qp with equality constraints an
 		std::cout << "total number of iteration: " << results.n_tot << std::endl;
 	}
 }
+
+DOCTEST_TEST_CASE("linear problem with equality  with equality constraints and linar cost and increasing dimension") {
+
+	std::cout << "---testing linear problem with equality constraints and increasing dimension---" << std::endl;
+	double  sparsity_factor = 0.15;
+	T eps_abs = T(1e-9);
+	for (isize dim = 10; dim < 1000; dim+=100) {
+
+		isize n_eq (dim / 2);
+		isize n_in (0);
+		T strong_convexity_factor(1.e-2);
+		Qp<T> qp{random_with_dim_and_neq_and_n_in, dim, n_eq, n_in, sparsity_factor, strong_convexity_factor};
+		qp.H.setZero();
+		auto y_sol = ldlt_test::rand::vector_rand<T>(n_eq); // make sure the LP is bounded within the feasible set
+		qp.g = - qp.A.transpose() * y_sol ;
+		qp::QPSettings<T> settings;
+		settings.eps_abs = eps_abs;
+		settings.verbose = false;
+		qp::QPData<T> data(dim, n_eq, n_in);
+		qp::QPResults<T> results{dim, n_eq, n_in};
+		qp::QPWorkspace<T> work{dim, n_eq, n_in};
+
+		qp::detail::QPsetup_dense<T>( //
+				qp.H,
+				qp.g,
+				qp.A,
+				qp.b,
+				qp.C,
+				qp.u,
+				qp.l,
+				settings,
+				data,
+				work,
+				results);
+
+		qp::detail::qp_solve(settings, data, results, work);
+		T pri_res = std::max((qp.A * results.x - qp.b).lpNorm<Eigen::Infinity>(), (qp::detail::positive_part(qp.C * results.x - qp.u) + qp::detail::negative_part(qp.C * results.x - qp.l) ).lpNorm<Eigen::Infinity>());
+		T dua_res = (qp.H * results.x + qp.g + qp.A.transpose() * results.y + qp.C.transpose() * results.z).lpNorm<Eigen::Infinity>();
+		DOCTEST_CHECK( pri_res <= eps_abs);
+		DOCTEST_CHECK( dua_res <= eps_abs);
+
+		std::cout << "------solving qp with dim: " << dim << " neq: " << n_eq  << " nin: " << n_in << std::endl;
+		std::cout << "primal residual: " << pri_res << std::endl;
+		std::cout << "dual residual: "  << dua_res << std::endl;
+		std::cout << "total number of iteration: " << results.n_tot << std::endl;
+	}
+}
--- test/src/qp_with_eq_and_in.cpp
+++ test/src/qp_with_eq_and_in.cpp
@@ -187,3 +187,49 @@ DOCTEST_TEST_CASE("sparse random strongly convex qp with degenerate inequality c
 		std::cout << "total number of iteration: " << results.n_tot << std::endl;
 	}
 }
+
+DOCTEST_TEST_CASE("linear problem with equality inequality constraints and increasing dimension") {
+
+	std::cout << "---testing linear problem with inequality constraints and increasing dimension---" << std::endl;
+	double  sparsity_factor = 0.15;
+	T eps_abs = T(1e-9);
+	T strong_convexity_factor(0.);
+	for (isize dim = 10; dim < 1000; dim+=100) {
+		isize n_in (dim / 2);
+		isize n_eq (0);
+		Qp<T> qp{random_with_dim_and_n_in_not_strongly_convex, dim, n_in, sparsity_factor};
+		qp.H.setZero();
+		auto z_sol = ldlt_test::rand::vector_rand<T>(n_in);
+		qp.g = -qp.C.transpose() * z_sol ; // make sure the LP is bounded within the feasible set
+		qp::QPSettings<T> settings;
+		settings.eps_abs = eps_abs;
+		settings.verbose = false;
+		qp::QPData<T> data(dim, n_eq, n_in);
+		qp::QPResults<T> results{dim, n_eq, n_in};
+		qp::QPWorkspace<T> work{dim, n_eq, n_in};
+
+		qp::detail::QPsetup_dense<T>( //
+				qp.H,
+				qp.g,
+				qp.A,
+				qp.b,
+				qp.C,
+				qp.u,
+				qp.l,
+				settings,
+				data,
+				work,
+				results);
+
+		qp::detail::qp_solve(settings, data, results, work);
+		T pri_res = std::max((qp.A * results.x - qp.b).lpNorm<Eigen::Infinity>(), (qp::detail::positive_part(qp.C * results.x - qp.u) + qp::detail::negative_part(qp.C * results.x - qp.l) ).lpNorm<Eigen::Infinity>());
+		T dua_res = (qp.H * results.x + qp.g + qp.A.transpose() * results.y + qp.C.transpose() * results.z).lpNorm<Eigen::Infinity>();
+		DOCTEST_CHECK( pri_res <= eps_abs);
+		DOCTEST_CHECK( dua_res <= eps_abs);
+
+		std::cout << "------solving qp with dim: " << dim << " neq: " << n_eq  << " nin: " << n_in << std::endl;
+		std::cout << "primal residual: " << pri_res << std::endl;
+		std::cout << "dual residual: "  << dua_res << std::endl;
+		std::cout << "total number of iteration: " << results.n_tot << std::endl;
+	}
+}
