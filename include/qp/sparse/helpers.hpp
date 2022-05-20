9252e9b2a3df5ca1f0fb87c3b2231f37f27845d3
BAMBADE Antoine <antoine.bambade@inria.fr>
2022-05-20 11:19:07 +0200

    dense and sparse: helpers files

---
+++ include/qp/sparse/helpers.hpp
@@ -0,0 +1,115 @@
+/** \file */
+#ifndef PROXSUITE_QP_SPARSE_HELPERS_HPP
+#define PROXSUITE_QP_SPARSE_HELPERS_HPP
+
+#include <Eigen/Sparse>
+#include <veg/vec.hpp>
+#include <qp/sparse/fwd.hpp>
+
+namespace proxsuite {
+namespace qp {
+namespace sparse {
+
+/*!
+* Update the proximal parameters of the results object.
+*
+* @param rho_new primal proximal parameter
+* @param mu_eq_new dual equality proximal parameter
+* @param mu_in_new dual inequality proximal parameter
+* @param results solver result 
+*/
+template <typename T>
+void update_proximal_parameters(
+		Results<T>& results,
+		tl::optional<T> rho_new,
+		tl::optional<T> mu_eq_new,
+		tl::optional<T> mu_in_new) {
+
+	if (rho_new != tl::nullopt) {
+		results.info.rho = rho_new.value();
+	}
+	if (mu_eq_new != tl::nullopt) {
+		results.info.mu_eq = mu_eq_new.value();
+		results.info.mu_eq_inv = T(1) / results.info.mu_eq;
+	}
+	if (mu_in_new != tl::nullopt) {
+		results.info.mu_in = mu_in_new.value();
+		results.info.mu_in_inv = T(1) / results.info.mu_in;
+	}
+}
+/*!
+* Warm start the results primal and dual variables.
+*
+* @param x_wm primal proximal parameter
+* @param y_wm dual equality proximal parameter
+* @param z_wm dual inequality proximal parameter
+* @param results solver result 
+* @param settings solver settings 
+*/
+template <typename T>
+void warm_start(
+		tl::optional<VecRef<T>> x_wm,
+		tl::optional<VecRef<T>> y_wm,
+		tl::optional<VecRef<T>> z_wm,
+		Results<T>& results,
+		Settings<T>& settings) {
+	bool real_wm = false;
+	if (x_wm != tl::nullopt) {
+		results.x = x_wm.value().eval();
+		real_wm = true;
+	}
+	if (y_wm != tl::nullopt) {
+		results.y = y_wm.value().eval();
+		real_wm = true;
+	}
+	if (z_wm != tl::nullopt) {
+		results.z = z_wm.value().eval();
+	}
+	if (real_wm) {
+		settings.warm_start = true;
+	}
+}
+
+template <typename T, typename I, typename P>
+void qp_setup(
+		QpView<T, I> qp,
+		Results<T>& results,
+		Model<T, I>& data,
+		Workspace<T, I>& work,
+		P& precond) {
+	isize n = qp.H.nrows();
+	isize n_eq = qp.AT.ncols();
+	isize n_in = qp.CT.ncols();
+
+	if (results.x.rows() != n) {
+		results.x.resize(n);
+		results.x.setZero();
+	}
+	if (results.y.rows() != n_eq) {
+		results.y.resize(n_eq);
+		results.y.setZero();
+	}
+	if (results.z.rows() != n_in) {
+		results.z.resize(n_in);
+		results.z.setZero();
+	}
+	if (results.active_constraints.len() != n_in) {
+		results.active_constraints.resize(n_in);
+		for (isize i = 0; i < n_in; ++i) {
+			results.active_constraints[i] = false;
+		}
+	}
+
+	work._.setup_impl(
+			qp,
+      results,
+			data,
+			precond,
+			P::scale_qp_in_place_req(veg::Tag<T>{}, n, n_eq, n_in));
+}
+
+} // namespace sparse
+} // namespace qp
+} // namespace proxsuite
+
+#endif /* end of include guard PROXSUITE_QP_SPARSE_HELPERS_HPP */
--- include/qp/sparse/wrapper.hpp
+++ include/qp/sparse/wrapper.hpp
@@ -7,114 +7,13 @@
 #include <tl/optional.hpp>
 #include <qp/results.hpp>
 #include <qp/settings.hpp>
-#include <qp/sparse/fwd.hpp>
 #include <qp/sparse/solver.hpp>
+#include <qp/sparse/helpers.hpp>
 
 namespace proxsuite {
 namespace qp {
 namespace sparse {
 
-////// SETUP
-////// UPDATES ///////
-
-/*!
-* Update the proximal parameters of the results object.
-*
-* @param rho_new primal proximal parameter
-* @param mu_eq_new dual equality proximal parameter
-* @param mu_in_new dual inequality proximal parameter
-* @param results solver result 
-*/
-template <typename T>
-void update_proximal_parameters(
-		Results<T>& results,
-		tl::optional<T> rho_new,
-		tl::optional<T> mu_eq_new,
-		tl::optional<T> mu_in_new) {
-
-	if (rho_new != tl::nullopt) {
-		results.info.rho = rho_new.value();
-	}
-	if (mu_eq_new != tl::nullopt) {
-		results.info.mu_eq = mu_eq_new.value();
-		results.info.mu_eq_inv = T(1) / results.info.mu_eq;
-	}
-	if (mu_in_new != tl::nullopt) {
-		results.info.mu_in = mu_in_new.value();
-		results.info.mu_in_inv = T(1) / results.info.mu_in;
-	}
-}
-/*!
-* Warm start the results primal and dual variables.
-*
-* @param x_wm primal proximal parameter
-* @param y_wm dual equality proximal parameter
-* @param z_wm dual inequality proximal parameter
-* @param results solver result 
-* @param settings solver settings 
-*/
-template <typename T>
-void warm_start(
-		tl::optional<VecRef<T>> x_wm,
-		tl::optional<VecRef<T>> y_wm,
-		tl::optional<VecRef<T>> z_wm,
-		Results<T>& results,
-		Settings<T>& settings) {
-	bool real_wm = false;
-	if (x_wm != tl::nullopt) {
-		results.x = x_wm.value().eval();
-		real_wm = true;
-	}
-	if (y_wm != tl::nullopt) {
-		results.y = y_wm.value().eval();
-		real_wm = true;
-	}
-	if (z_wm != tl::nullopt) {
-		results.z = z_wm.value().eval();
-	}
-	if (real_wm) {
-		settings.warm_start = true;
-	}
-}
-
-template <typename T, typename I, typename P>
-void qp_setup(
-		QpView<T, I> qp,
-		Results<T>& results,
-		Model<T, I>& data,
-		Workspace<T, I>& work,
-		P& precond) {
-	isize n = qp.H.nrows();
-	isize n_eq = qp.AT.ncols();
-	isize n_in = qp.CT.ncols();
-
-	if (results.x.rows() != n) {
-		results.x.resize(n);
-		results.x.setZero();
-	}
-	if (results.y.rows() != n_eq) {
-		results.y.resize(n_eq);
-		results.y.setZero();
-	}
-	if (results.z.rows() != n_in) {
-		results.z.resize(n_in);
-		results.z.setZero();
-	}
-	if (results.active_constraints.len() != n_in) {
-		results.active_constraints.resize(n_in);
-		for (isize i = 0; i < n_in; ++i) {
-			results.active_constraints[i] = false;
-		}
-	}
-
-	work._.setup_impl(
-			qp,
-      results,
-			data,
-			precond,
-			P::scale_qp_in_place_req(veg::Tag<T>{}, n, n_eq, n_in));
-}
-///// QP object
 template <typename T, typename I>
 struct QP {
 	Results<T> results;
@@ -159,7 +58,7 @@ struct QP {
 	};
 
 	void solve() {
-		sparse::qp_solve( //
+			qp_solve( //
 				results,
 				data,
 				settings,
@@ -169,13 +68,13 @@ struct QP {
 
 	void update_proximal_parameters(
 			tl::optional<T> rho, tl::optional<T> mu_eq, tl::optional<T> mu_in) {
-		proxsuite::qp::sparse::update_proximal_parameters(results, rho, mu_eq, mu_in);
+		update_proximal_parameters(results, rho, mu_eq, mu_in);
 	};
 	void warm_start(
 			tl::optional<VecRef<T>> x,
 			tl::optional<VecRef<T>> y,
 			tl::optional<VecRef<T>> z) {
-		proxsuite::qp::sparse::warm_start(x, y, z, results, settings);
+		warm_start(x, y, z, results, settings);
 	};
 	void cleanup() { results.cleanup(); }
 };
@@ -220,10 +119,10 @@ qp::Results<T> solve(
 	isize n_in = C.value().rows();
 
 	qp::sparse::QP<T, I> Qp(n, n_eq, n_in);
-	Qp.setup_sparse_matrices(
+	Qp.setup(
 			H, g, A, b, C, u, l); // symbolic factorisation done here
 
-	Qp.update_prox_parameter(rho, mu_eq, mu_in);
+	Qp.update_proximal_parameters(rho, mu_eq, mu_in);
 	Qp.warm_start(x, y, z);
 
 	if (eps_abs != tl::nullopt) {
