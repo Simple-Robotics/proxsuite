f106e94660aab66617e6c5a5f9aa6fc2a52fcc08
Justin Carpentier <justin.carpentier@inria.fr>
2022-07-29 17:15:00 +0200

    python: expose version helpers

--- bindings/python/src/expose-all.cpp
+++ bindings/python/src/expose-all.cpp
@@ -7,6 +7,7 @@
 
 #include "algorithms.hpp"
 #include <proxsuite/proxqp/dense/utils.hpp>
+#include <proxsuite/helpers/version.hpp>
 
 namespace proxsuite {
 namespace proxqp {
@@ -50,6 +51,19 @@ PYBIND11_MODULE(PYTHON_MODULE_NAME, m) {
   exposeDenseAlgorithms<f64>(dense_module);
     pybind11::module_ sparse_module = proxqp_module.def_submodule("sparse", "Sparse solver of proxQP");
 	exposeSparseAlgorithms<f64,int32_t>(sparse_module);
+    
+  // Add version
+  m.attr("__version__") = helpers::printVersion();
+  
+  // Add helpers
+  pybind11::module_ helpers_module = m.def_submodule("helpers", "Helper module");
+  helpers_module.def("printVersion", helpers::printVersion,
+                     pybind11::arg("delimiter") = ".",
+                     "Print the current version of the package.");
+  helpers_module.def("checkVersionAtLeast", helpers::checkVersionAtLeast,
+                     pybind11::arg("major_version"),pybind11::arg("minor_version"),pybind11::arg("patch_version"),
+                     "Check version of the package is at least greater than the one provided as input.");
+
     
 }
 
