a0f16aa750e430d9d4e7dbac80438660603d2287
Justin Carpentier <justin.carpentier@inria.fr>
2022-08-01 18:02:29 +0200

    cmake: fix convention for Linux

--- cmake-external/FindSimde.cmake
+++ cmake-external/FindSimde.cmake
@@ -1,10 +1,10 @@
-find_path(SIMDE_INCLUDE_DIR simde/simde-math.h)
+find_path(Simde_INCLUDE_DIR simde/simde-math.h)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
-  simde
-  FOUND_VAR SIMDE_FOUND
-  REQUIRED_VARS SIMDE_INCLUDE_DIR
+  Simde
+  FOUND_VAR Simde_FOUND
+  REQUIRED_VARS Simde_INCLUDE_DIR
 )
 
 if(SIMDE_FOUND)
@@ -18,7 +18,7 @@ mark_as_advanced(SIMDE_INCLUDE_DIR)
 
 include(FeatureSummary)
 set_package_properties(
-  simde PROPERTIES
+  Simde PROPERTIES
   DESCRIPTION
     "Implementations of SIMD instruction sets for systems which don't natively support them."
   URL "https://github.com/simd-everywhere/simde"
