daf889a4ad4fabce4c67e87b8db4ddb5a5fcdf7c
Justin Carpentier <justin.carpentier@inria.fr>
2022-08-06 19:06:46 +0200

    cmake/external: fix FindSimde

--- cmake-external/FindSimde.cmake
+++ cmake-external/FindSimde.cmake
@@ -8,15 +8,12 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   Simde
   FOUND_VAR Simde_FOUND
-  REQUIRED_VARS Simde_INCLUDE_DIR
-  REASON_FAILURE_MESSAGE ${SIMDE_HINT_FAILURE}
-)
+  REQUIRED_VARS Simde_INCLUDE_DIR REASON_FAILURE_MESSAGE ${SIMDE_HINT_FAILURE})
 
-if(SIMDE_FOUND)
+if(Simde_FOUND)
   add_library(simde INTERFACE IMPORTED)
-  set_target_properties(
-    simde PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${Simde_INCLUDE_DIR}"
-  )
+  set_target_properties(simde PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+                                         "${Simde_INCLUDE_DIR}")
 endif()
 
 mark_as_advanced(SIMDE_INCLUDE_DIR)
@@ -26,5 +23,4 @@ set_package_properties(
   Simde PROPERTIES
   DESCRIPTION
     "Implementations of SIMD instruction sets for systems which don't natively support them."
-  URL "https://github.com/simd-everywhere/simde"
-)
+  URL "https://github.com/simd-everywhere/simde")
