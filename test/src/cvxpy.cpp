10370f9950496c719776042f861b9abd37c7a3be
fabinsch <fabi.schramm@gmail.com>
2022-09-01 17:43:00 +0200

    cpp/test: add cvxpy testcases

---
+++ test/src/cvxpy.cpp
@@ -0,0 +1,124 @@
+//
+// Copyright (c) 2022 INRIA
+//
+#include <doctest.hpp>
+#include <Eigen/Core>
+#include <proxsuite/proxqp/dense/dense.hpp>
+
+using T = double;
+using namespace proxsuite;
+using namespace proxsuite::proxqp;
+
+template<typename T, proxqp::Layout L>
+using Mat =
+  Eigen::Matrix<T,
+                Eigen::Dynamic,
+                Eigen::Dynamic,
+                (L == proxqp::colmajor) ? Eigen::ColMajor : Eigen::RowMajor>;
+template<typename T>
+using Vec = Eigen::Matrix<T, Eigen::Dynamic, 1>;
+
+DOCTEST_TEST_CASE("3 dim test case from cvxpy, check feasibility")
+{
+
+  std::cout << "---3 dim test case from cvxpy, check feasibility "
+            << std::endl;
+  T eps_abs = T(1e-9);
+  dense::isize dim = 3;
+
+  Mat<T, colmajor> H = Mat<T, colmajor>(dim, dim);
+  H << 13.0, 12.0, -2.0,
+       12.0, 17.0, 6.0, 
+       -2.0, 6.0, 12.0;
+
+  Vec<T> g = Vec<T>(dim);
+  g << -22.0, -14.5, 13.0;
+
+  Mat<T, colmajor> C = Mat<T, colmajor>(dim, dim);
+  C << 1.0, 0.0, 0.0,
+       0.0, 1.0, 0.0, 
+       0.0, 0.0, 1.0;
+
+  Vec<T> l = Vec<T>(dim);
+  l << -1.0, -1.0, -1.0;
+
+  Vec<T> u = Vec<T>(dim);
+  u << 1.0, 1.0, 1.0;
+  Results<T> results = dense::solve<T>(H,
+                                       g,
+                                       std::nullopt,
+                                       std::nullopt,
+                                       C,
+                                       u,
+                                       l,
+                                       std::nullopt,
+                                       std::nullopt,
+                                       std::nullopt,
+                                       eps_abs,
+                                       0);
+
+  T pri_res = (dense::positive_part(C * results.x - u) +
+               dense::negative_part(C * results.x - l)).lpNorm<Eigen::Infinity>();
+  T dua_res = (H * results.x + g + C.transpose() * results.z).lpNorm<Eigen::Infinity>();
+  DOCTEST_CHECK(pri_res <= eps_abs);
+  DOCTEST_CHECK(dua_res <= eps_abs);
+
+
+  std::cout << "primal residual: " << pri_res << std::endl;
+  std::cout << "dual residual: " << dua_res << std::endl;
+  std::cout << "total number of iteration: " << results.info.iter << std::endl;
+  std::cout << "setup timing " << results.info.setup_time << " solve time "
+            << results.info.solve_time << std::endl;
+}
+
+DOCTEST_TEST_CASE("simple test case from cvxpy, check feasibility")
+{
+
+  std::cout << "---simple test case from cvxpy, check feasibility "
+            << std::endl;
+  T eps_abs = T(1e-4);
+  dense::isize dim = 1;
+
+  Mat<T, colmajor> H = Mat<T, colmajor>(dim, dim);
+  H << 20.0;
+
+  Vec<T> g = Vec<T>(dim);
+  g << -10.0;
+
+  Mat<T, colmajor> C = Mat<T, colmajor>(dim, dim);
+  C << 1.0;
+
+  Vec<T> l = Vec<T>(dim);
+  l << 0.0;
+
+  Vec<T> u = Vec<T>(dim);
+  u << 1.0;
+  Results<T> results = dense::solve<T>(H,
+                                       g,
+                                       std::nullopt,
+                                       std::nullopt,
+                                       C,
+                                       u,
+                                       l,
+                                       std::nullopt,
+                                       std::nullopt,
+                                       std::nullopt,
+                                       eps_abs,
+                                       0);
+
+  T pri_res = (dense::positive_part(C * results.x - u) +
+               dense::negative_part(C * results.x - l)).lpNorm<Eigen::Infinity>();
+  T dua_res = (H * results.x + g + C.transpose() * results.z).lpNorm<Eigen::Infinity>();
+  T x_sol = 0.5;
+  
+  DOCTEST_CHECK((x_sol - results.x.coeff(0,0)) <= eps_abs);
+  DOCTEST_CHECK(pri_res <= eps_abs);
+  DOCTEST_CHECK(dua_res <= eps_abs);
+
+
+  std::cout << "primal residual: " << pri_res << std::endl;
+  std::cout << "dual residual: " << dua_res << std::endl;
+  std::cout << "total number of iteration: " << results.info.iter << std::endl;
+  std::cout << "setup timing " << results.info.setup_time << " solve time "
+            << results.info.solve_time << std::endl;
+}
