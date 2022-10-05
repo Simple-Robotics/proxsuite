0e24d27ecb72b76c25933afc9183138ec95540cf
Bambade <antoine.bambade@inria.fr>
2022-10-05 19:37:50 +0200

    remove commented code

--- examples/cpp/solve_without_api_and_option.cpp
+++ examples/cpp/solve_without_api_and_option.cpp
@@ -12,18 +12,6 @@ main()
   isize n = 10;
   isize n_eq(n / 4);
   isize n_in(n / 4);
-
-  //T p = 0.15;            // level of sparsity
-  //T conditioning = 10.0; // conditioning level for H
-  //auto H = ::proxsuite::proxqp::utils::rand::sparse_positive_definite_rand(
-  //  n, conditioning, p);
-  //auto g = ::proxsuite::proxqp::utils::rand::vector_rand<T>(n);
-  //auto A = ::proxsuite::proxqp::utils::rand::sparse_matrix_rand<T>(n_eq, n, p);
-  //auto C = ::proxsuite::proxqp::utils::rand::sparse_matrix_rand<T>(n_in, n, p);
-  //auto x_sol = ::proxsuite::proxqp::utils::rand::vector_rand<T>(n);
-  //auto b = A * x_sol;
-  //auto l = C * x_sol;
-  //auto u = (l.array() + 10).matrix().eval();
   T sparsity_factor(0.15);
   T strong_convexity_factor(1.e-2);
   dense::Model<T> qp_random = utils::dense_strongly_convex_qp(
