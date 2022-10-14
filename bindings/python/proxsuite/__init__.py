47c5759aaf1d76a9254c4c73c81cb5fd8e754e23
Fabian Schramm <55981657+fabinsch@users.noreply.github.com>
2022-10-14 14:08:45 +0200

    arm: do not use instruction set

--- bindings/python/proxsuite/__init__.py
+++ bindings/python/proxsuite/__init__.py
@@ -1,4 +1,11 @@
-from . import instructionset
+import platform
+
+if (
+    "i386" in platform.processor()
+    or "x86_64" in platform.processor()
+    or "Intel64" in platform.processor()
+):
+    from . import instructionset
 
 
 def load_main_module(globals):
@@ -13,17 +20,19 @@ def load_main_module(globals):
         except ModuleNotFoundError:
             return False
 
-    all_modules = [
-        ("proxsuite_pywrap_avx512", instructionset.has_AVX512F),
-        ("proxsuite_pywrap_avx2", instructionset.has_AVX2),
-    ]
+    if "arm" not in platform.processor():
+        all_modules = [
+            ("proxsuite_pywrap_avx512", instructionset.has_AVX512F),
+            ("proxsuite_pywrap_avx2", instructionset.has_AVX2),
+        ]
 
-    for module_name, checker in all_modules:
-        if checker() and load_module(module_name):
-            return
+        for module_name, checker in all_modules:
+            if checker() and load_module(module_name):
+                return
 
     assert load_module("proxsuite_pywrap") == True
 
 
 load_main_module(globals=globals())
 del load_main_module
+del platform
