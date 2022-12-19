2c1b6cb4f6c632e1c32ca94b597f9d868d886c48
Fabian Schramm <55981657+fabinsch@users.noreply.github.com>
2022-12-19 17:52:10 +0100

    unittest: save qp model to new dir and load it

--- test/src/dense_qp_wrapper.cpp
+++ test/src/dense_qp_wrapper.cpp
@@ -6799,3 +6799,33 @@ TEST_CASE("ProxQP::dense: init must be called before update")
   CHECK(dua_res <= eps_abs);
   CHECK(pri_res <= eps_abs);
 }
+
+#ifdef PROXSUITE_WITH_SERIALIZATION
+
+DOCTEST_TEST_CASE("ProxQP::dense: test serialization")
+{
+  std::cout << "--- serialization ---" << std::endl;
+  double sparsity_factor = 0.15;
+  T eps_abs = T(1e-9);
+  utils::rand::set_seed(1);
+  dense::isize dim = 10;
+
+  dense::isize n_eq(0);
+  dense::isize n_in(dim / 4);
+  T strong_convexity_factor(1.e-2);
+  proxqp::dense::Model<T> qp_random = proxqp::utils::dense_strongly_convex_qp(
+    dim, n_eq, n_in, sparsity_factor, strong_convexity_factor);
+
+  dense::QP<T> qp{ dim, n_eq, n_in }; // creating QP object
+  qp.init(qp_random.H,
+          qp_random.g,
+          qp_random.A,
+          qp_random.b,
+          qp_random.C,
+          qp_random.l,
+          qp_random.u);
+
+  qp.saveToBinary("tmp");
+  qp.loadFromBinary("tmp");
+}
+#endif
