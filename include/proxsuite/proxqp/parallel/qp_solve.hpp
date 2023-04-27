bf1dd2a5ec24489895fd1fd3f7ccd9ff23b0f882
Fabian Schramm <55981657+fabinsch@users.noreply.github.com>
2023-04-27 17:27:10 +0200

    qp parallel solve for VectorQP

--- include/proxsuite/proxqp/parallel/qp_solve.hpp
+++ include/proxsuite/proxqp/parallel/qp_solve.hpp
@@ -30,6 +30,51 @@ qp_solve_in_parallel(const size_t num_threads,
   }
 }
 
+template<typename T>
+void
+qp_solve_in_parallel(optional<const size_t> num_threads,
+                     proxqp::dense::VectorQP<T>& qps)
+{
+  if (num_threads != nullopt) {
+    set_default_omp_options(num_threads.value());
+  } else {
+    size_t NUM_THREADS =
+      std::max((size_t)(omp_get_max_threads() / 2 - 2), (size_t)(1));
+    set_default_omp_options(NUM_THREADS);
+  }
+  typedef proxqp::dense::QP<T> qp_dense;
+  const Eigen::DenseIndex batch_size = qps.size();
+  Eigen::DenseIndex i = 0;
+#pragma omp parallel for schedule(dynamic)
+  for (i = 0; i < batch_size; i++) {
+    qp_dense& qp = qps[i];
+    qp.solve();
+  }
+}
+
+template<typename T, typename I>
+void
+qp_solve_in_parallel(optional<const size_t> num_threads,
+                     proxqp::sparse::VectorQP<T, I>& qps)
+{
+  if (num_threads != nullopt) {
+    set_default_omp_options(num_threads.value());
+  } else {
+    size_t NUM_THREADS =
+      std::max((size_t)(omp_get_max_threads() / 2 - 2), (size_t)(1));
+    set_default_omp_options(NUM_THREADS);
+  }
+
+  typedef proxqp::sparse::QP<T, I> qp_sparse;
+  const Eigen::DenseIndex batch_size = qps.size();
+  Eigen::DenseIndex i = 0;
+#pragma omp parallel for schedule(dynamic)
+  for (i = 0; i < batch_size; i++) {
+    qp_sparse& qp = qps[i];
+    qp.solve();
+  }
+}
+
 template<typename T, typename I>
 void
 qp_solve_in_parallel(const size_t num_threads,
@@ -46,6 +91,7 @@ qp_solve_in_parallel(const size_t num_threads,
     qp.solve();
   }
 }
+
 }
 }
 } // namespace proxsuite::proxqp::parallel
