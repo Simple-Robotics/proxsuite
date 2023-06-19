7846cee07f8154ad4f0beb56ee3a351c67d0eed2
Fabian Schramm <55981657+fabinsch@users.noreply.github.com>
2023-06-19 18:09:08 +0200

    modify handling of num_threads

--- include/proxsuite/proxqp/parallel/qp_solve.hpp
+++ include/proxsuite/proxqp/parallel/qp_solve.hpp
@@ -18,13 +18,14 @@ void
 solve_in_parallel(std::vector<proxqp::dense::QP<T>>& qps,
                   optional<const size_t> num_threads = nullopt)
 {
+  size_t NUM_THREADS =
+    std::max((size_t)(omp_get_max_threads() / 2),
+             (size_t)(1)); // TODO(jcarpent): find optimal dispatch
   if (num_threads != nullopt) {
-    set_default_omp_options(num_threads.value());
-  } else {
-    size_t NUM_THREADS =
-      std::max((size_t)(omp_get_max_threads() / 2), (size_t)(1));
-    set_default_omp_options(NUM_THREADS);
+    NUM_THREADS = num_threads.value();
   }
+  set_default_omp_options(NUM_THREADS);
+
   typedef proxqp::dense::QP<T> qp_dense;
   const Eigen::DenseIndex batch_size = qps.size();
   Eigen::DenseIndex i = 0;
@@ -40,13 +41,13 @@ void
 solve_in_parallel(proxqp::dense::BatchQP<T>& qps,
                   optional<const size_t> num_threads = nullopt)
 {
+  size_t NUM_THREADS =
+    std::max((size_t)(omp_get_max_threads() / 2), (size_t)(1));
   if (num_threads != nullopt) {
-    set_default_omp_options(num_threads.value());
-  } else {
-    size_t NUM_THREADS =
-      std::max((size_t)(omp_get_max_threads() / 2), (size_t)(1));
-    set_default_omp_options(NUM_THREADS);
+    NUM_THREADS = num_threads.value();
   }
+  set_default_omp_options(NUM_THREADS);
+
   typedef proxqp::dense::QP<T> qp_dense;
   const Eigen::DenseIndex batch_size = qps.size();
   Eigen::DenseIndex i = 0;
@@ -64,13 +65,12 @@ void
 solve_in_parallel(proxqp::sparse::BatchQP<T, I>& qps,
                   optional<const size_t> num_threads = nullopt)
 {
+  size_t NUM_THREADS =
+    std::max((size_t)(omp_get_max_threads() / 2), (size_t)(1));
   if (num_threads != nullopt) {
-    set_default_omp_options(num_threads.value());
-  } else {
-    size_t NUM_THREADS =
-      std::max((size_t)(omp_get_max_threads() / 2), (size_t)(1));
-    set_default_omp_options(NUM_THREADS);
+    NUM_THREADS = num_threads.value();
   }
+  set_default_omp_options(NUM_THREADS);
 
   typedef proxqp::sparse::QP<T, I> qp_sparse;
   const Eigen::DenseIndex batch_size = qps.size();
@@ -87,13 +87,12 @@ void
 solve_in_parallel(std::vector<proxqp::sparse::QP<T, I>>& qps,
                   optional<const size_t> num_threads = nullopt)
 {
+  size_t NUM_THREADS =
+    std::max((size_t)(omp_get_max_threads() / 2), (size_t)(1));
   if (num_threads != nullopt) {
-    set_default_omp_options(num_threads.value());
-  } else {
-    size_t NUM_THREADS =
-      std::max((size_t)(omp_get_max_threads() / 2), (size_t)(1));
-    set_default_omp_options(NUM_THREADS);
+    NUM_THREADS = num_threads.value();
   }
+  set_default_omp_options(NUM_THREADS);
 
   typedef proxqp::sparse::QP<T, I> qp_sparse;
   const Eigen::DenseIndex batch_size = qps.size();
