63a9561a7186c263e618ba882f6a096885cfbe84
Fabian Schramm <55981657+fabinsch@users.noreply.github.com>
2024-03-07 17:37:28 +0100

    qplayer: replace torch.Tensor
    - either use torch.tensor or torch.empty

--- bindings/python/proxsuite/torch/qplayer.py
+++ bindings/python/proxsuite/torch/qplayer.py
@@ -114,9 +114,9 @@ def QPFunction(
             if ctx.cpu is not None:
                 ctx.cpu = max(1, int(ctx.cpu / 2))
 
-            zhats = torch.empty((nBatch, ctx.nz)).type_as(Q)
-            lams = torch.empty((nBatch, ctx.neq)).type_as(Q)
-            nus = torch.empty((nBatch, ctx.nineq)).type_as(Q)
+            zhats = torch.empty((nBatch, ctx.nz), dtype=Q.dtype)
+            lams = torch.empty((nBatch, ctx.neq), dtype=Q.dtype)
+            nus = torch.empty((nBatch, ctx.nineq), dtype=Q.dtype)
 
             for i in range(nBatch):
                 qp = ctx.vector_of_qps.init_qp_in_place(ctx.nz, ctx.neq, ctx.nineq)
@@ -272,20 +272,20 @@ def QPFunction(
             assert neq > 0 or nineq > 0
             ctx.neq, ctx.nineq, ctx.nz = neq, nineq, nz
 
-            zhats = torch.Tensor(nBatch, ctx.nz).type_as(Q)
-            nus = torch.Tensor(nBatch, ctx.nineq).type_as(Q)
+            zhats = torch.empty((nBatch, ctx.nz), dtype=Q.dtype)
+            nus = torch.empty((nBatch, ctx.nineq), dtype=Q.dtype)
             lams = (
-                torch.Tensor(nBatch, ctx.neq).type_as(Q)
+                torch.empty(nBatch, ctx.neq, dtype=Q.dtype)
                 if ctx.neq > 0
-                else torch.Tensor()
+                else torch.empty()
             )
             s_e = (
-                torch.Tensor(nBatch, ctx.neq).type_as(Q)
+                torch.empty(nBatch, ctx.neq, dtype=Q.dtype)
                 if ctx.neq > 0
-                else torch.Tensor()
+                else torch.empty()
             )
-            slacks = torch.Tensor(nBatch, ctx.nineq).type_as(Q)
-            s_i = torch.Tensor(nBatch, ctx.nineq).type_as(Q)
+            slacks = torch.empty((nBatch, ctx.nineq), dtype=Q.dtype)
+            s_i = torch.empty((nBatch, ctx.nineq), dtype=Q.dtype)
 
             vector_of_qps = proxsuite.proxqp.dense.BatchQP()
 
@@ -338,13 +338,13 @@ def QPFunction(
 
             for i in range(nBatch):
                 si = -h[i] + G[i] @ vector_of_qps.get(i).results.x
-                zhats[i] = torch.Tensor(vector_of_qps.get(i).results.x)
-                nus[i] = torch.Tensor(vector_of_qps.get(i).results.z)
-                slacks[i] = torch.Tensor(si)
+                zhats[i] = torch.tensor(vector_of_qps.get(i).results.x)
+                nus[i] = torch.tensor(vector_of_qps.get(i).results.z)
+                slacks[i] = si.clone().detach()
                 if neq > 0:
-                    lams[i] = torch.Tensor(vector_of_qps.get(i).results.y)
-                    s_e[i] = torch.Tensor(vector_of_qps.get(i).results.se)
-                s_i[i] = torch.Tensor(vector_of_qps.get(i).results.si)
+                    lams[i] = torch.tensor(vector_of_qps.get(i).results.y)
+                    s_e[i] = torch.tensor(vector_of_qps.get(i).results.se)
+                s_i[i] = torch.tensor(vector_of_qps.get(i).results.si)
 
             ctx.lams = lams
             ctx.nus = nus
