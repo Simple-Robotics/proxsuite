d71966317a12ec943ce4702135c3d90828924899
ManifoldFR <wilson.jallet@polytechnique.org>
2024-08-30 12:05:15 +0200

    Fix for nanobind v2.1.0 release

--- bindings/python/src/optional-eigen-fix.hpp
+++ bindings/python/src/optional-eigen-fix.hpp
@@ -14,14 +14,18 @@ NAMESPACE_BEGIN(detail)
 /// https://github.com/wjakob/nanobind/issues/682#issuecomment-2310746145
 template<typename T>
 struct type_caster<std::optional<Eigen::Ref<const T>>>
-  : optional_caster<std::optional<Eigen::Ref<const T>>>
 {
   using Ref = Eigen::Ref<const T>;
   using Optional = std::optional<Ref>;
-  using Caster = typename type_caster::optional_caster::Caster;
+  using Caster = make_caster<Ref>;
   using Map = typename Caster::Map;
   using DMap = typename Caster::DMap;
   NB_TYPE_CASTER(Optional, optional_name(Caster::Name))
+
+  type_caster()
+    : value(std::nullopt)
+  {
+  }
 
   bool from_python(handle src, uint8_t flags, cleanup_list* cleanup) noexcept
   {
@@ -38,6 +42,17 @@ struct type_caster<std::optional<Eigen::Ref<const T>>>
     else
       value.emplace(caster.caster.operator Map());
     return true;
+  }
+
+  template<typename T_>
+  static handle from_cpp(T_&& value,
+                         rv_policy policy,
+                         cleanup_list* cleanup) noexcept
+  {
+    if (!value)
+      return none().release();
+
+    return Caster::from_cpp(forward_like_<T_>(*value), policy, cleanup);
   }
 };
 
