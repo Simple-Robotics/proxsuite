28a1a1b29eebf51200fdb587f4a98ec48a573615
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-02-11 19:53:01 +0100

    remove unused import

--- examples/python/qplayer_sudoku.py
+++ examples/python/qplayer_sudoku.py
@@ -8,7 +8,6 @@ try:
     import torch
     import torch.nn as nn
     import torch.optim as optim
-    from torch.autograd import Variable
     from torch.nn.parameter import Parameter
 
     import cvxpy as cp
