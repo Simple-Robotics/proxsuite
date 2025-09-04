db0936bc6340aa5010e3f49a512557c53ba66bc9
Joris Vaillant <joris.vaillant@inria.fr>
2025-09-04 11:33:33 +0200

    packaging: Fix build

--- test/packaging/src/run-proxqp.cpp
+++ test/packaging/src/run-proxqp.cpp
@@ -1,29 +1,29 @@
 #include <proxsuite/proxqp/dense/dense.hpp> // load the dense solver backend
 #include <proxsuite/common/utils/random_qp_problems.hpp> // used for generating a random convex Qp
 
-using namespace proxsuite::proxqp;
+using namespace proxsuite;
 using T = double;
 
 int
 main()
 {
-  isize dim = 10;
-  isize n_eq(dim / 4);
-  isize n_in(dim / 4);
+  common::isize dim = 10;
+  common::isize n_eq(dim / 4);
+  common::isize n_in(dim / 4);
   // generate a random qp
   T sparsity_factor(0.15);
   T strong_convexity_factor(1.e-2);
 
-  dense::Model<T> qp = utils::dense_strongly_convex_qp(
+  common::dense::Model<T> qp = common::utils::dense_strongly_convex_qp(
     dim, n_eq, n_in, sparsity_factor, strong_convexity_factor);
 
-  dense::QP<T> Qp(dim, n_eq, n_in);                  // create the QP object
+  proxqp::dense::QP<T> Qp(dim, n_eq, n_in);          // create the QP object
   Qp.init(qp.H, qp.g, qp.A, qp.b, qp.C, qp.l, qp.u); // initialize the model
   Qp.solve(); // solve the problem without warm start
 
-  auto x_wm = utils::rand::vector_rand<T>(dim);
-  auto y_wm = utils::rand::vector_rand<T>(n_eq);
-  auto z_wm = utils::rand::vector_rand<T>(n_in);
+  auto x_wm = common::utils::rand::vector_rand<T>(dim);
+  auto y_wm = common::utils::rand::vector_rand<T>(n_eq);
+  auto z_wm = common::utils::rand::vector_rand<T>(n_in);
   Qp.solve(x_wm, y_wm,
            z_wm); // if you have a warm start, put it here
 
