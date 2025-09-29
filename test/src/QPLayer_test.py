af8c8766e52df7eed176bf63c58f905cfe936a8b
Mathis SCHEFFLER <99043449+oomcth@users.noreply.github.com>
2025-09-29 16:29:49 +0200

    Fixed a ruff error

--- test/src/QPLayer_test.py
+++ test/src/QPLayer_test.py
@@ -120,8 +120,8 @@ def solve_single_qp_torch_non_structural_feasible(
     if not eq:
         A = b = torch.tensor([])
     if not neq:
-        C = u = l = torch.tensor([])
-    output_torch = function(P, q, A, b, C, l, u)
+        G = ub = lb = torch.tensor([])
+    output_torch = function(P, q, A, b, G, lb, ub)
     return output_torch[0]
 
 
