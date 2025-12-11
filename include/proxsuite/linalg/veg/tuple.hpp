c3ffe9a94af1360e2ec48a78b1332695daf8694e
Antoine Hoarau <703240+ahoarau@users.noreply.github.com>
2025-12-11 12:38:43 +0100

    remove no longer needed condition in c++17

--- include/proxsuite/linalg/veg/tuple.hpp
+++ include/proxsuite/linalg/veg/tuple.hpp
@@ -754,16 +754,9 @@ private:
     proxsuite::linalg::veg::meta::false_type /*unused*/,
     Tuples&&... tups) VEG_NOEXCEPT -> Concat<Tuples...>
   {
-#if defined(_MSC_VER) || (defined(__clang__) && __clang_major__ >= 19) ||      \
-  (defined(__APPLE__) && defined(__clang__) && __clang_major__ >= 17)
     return cat::from_ref_to_result(
       Tag<proxsuite::linalg::veg::meta::type_sequence_cat<Tuple, Tuples...>>{},
       cat::apply(_detail::_tuple::tuple_fwd(VEG_FWD(tups))...));
-#else
-    return cat::template from_ref_to_result(
-      Tag<proxsuite::linalg::veg::meta::type_sequence_cat<Tuple, Tuples...>>{},
-      cat::apply(_detail::_tuple::tuple_fwd(VEG_FWD(tups))...));
-#endif
   }
 
   template<typename... Targets, usize... Is, typename... Refs>
